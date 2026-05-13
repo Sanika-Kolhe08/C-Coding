c6441b9d402bfb3529523f519e8868d8cdbdedea
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-05-13 21:45:36 +0530

    Add files via upload

---
+++ FunctionDemoXX.c
@@ -0,0 +1,17 @@
+//FunctionDemoXX.c is updated file of FunctionDemoX.c
+//Capital X denotes updated file
+#include <stdio.h>
+
+void display()
+{
+     printf("Jay Ganesh....\n");
+}
+
+int main()
+{   
+    display();
+    display();
+    display();
+
+    return 0;
+}
